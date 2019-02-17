b4984159252721e3a5a73acabda42b4cb793a6e0
Tiffany Ray <tiffanyraylee@gmail.com>
2019-02-17 13:58:09 -0600

    removed unnecessary code

--- docs/src/script.js
+++ docs/src/script.js
@@ -5,21 +5,6 @@ $(document).ready(function() {
 
 var form = document.getElementById('quizForm');
 
-const getRadioVal = (name) => {
-    var val;
-    // get list of radio buttons with specified name
-    var radios = form.elements[name];
-    
-    // loop through list of radio buttons
-    for (var i=0, len=radios.length; i<len; i++) {
-        if ( radios[i].checked ) { // radio checked?
-            val = radios[i].value; // if so, hold its value in val
-            break; // and break out of for loop
-        }
-    }
-    return val; // return value of checked radio or undefined if none checked
-};
-
 //Event handler to listen for submit button
 form.addEventListener('submit', (e) => {
     e.preventDefault();
